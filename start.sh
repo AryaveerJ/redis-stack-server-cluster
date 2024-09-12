6ee9bc87f40293a8c6a1d822df5afc579bd87281
Palem Jagadeesh <palem.jagadeesh@amagi.com>
2024-09-12 20:48:18 +0000

    add a quick fix to port forward all the ports of master as some client libraries send request directly to corresponding nodes based on slots and nodes

--- start.sh
+++ start.sh
@@ -3,6 +3,8 @@
 # Exit script on any error
 set -e
 
+# Always send ports different than 6379,6380,6381
+# Add a valiadtion later
 # Accept Redis master ports as arguments
 MASTER1_PORT=$1
 MASTER2_PORT=$2
@@ -13,5 +15,8 @@ docker run -d \
   -p ${MASTER1_PORT}:6379 \
   -p ${MASTER2_PORT}:6380 \
   -p ${MASTER3_PORT}:6381 \
+  -p 6379:6379 \
+  -p 6380:6380 \
+  -p 6381:6381 \
   --name redis-cluster \
   aryaveerj/redis-stack-server-cluster:1.0.0
